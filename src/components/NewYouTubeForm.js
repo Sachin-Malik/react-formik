54cb105e6d1acda36f001a3cfc2162d6ea63b9d5
Sachin Malik <sachinmalik199821@gmail.com>
2022-02-06 14:09:32 +0530

    More Properties of Formik

--- src/components/NewYouTubeForm.js
+++ src/components/NewYouTubeForm.js
@@ -1,15 +1,24 @@
 import React from 'react';
-import {Formik, Form, Field, ErrorMessage} from 'formik'
+import {Formik, Form, Field, ErrorMessage, FieldArray} from 'formik'
 import * as Yup from 'yup'
+import TextError from './TextError';
 
 const initialValues ={
     name:'',
     email:'',
-    channel:''
+    channel:'',
+    comments:'',
+    social:{
+      facebook:'',
+      twitter:'',
+    },
+    phoneNumbers:['',''],
+    phNumbers:[''],
 };
 
-const onSubmit=values=>{
+const onSubmit=(values,onSubmitProps)=>{
     console.log('Values',values);
+    onSubmitProps.setSubmitting(false);
 }
 
 const validationSchema = Yup.object({
@@ -18,6 +27,13 @@ const validationSchema = Yup.object({
    channel:Yup.string().required('This is Required')
 })
 
+const validateComments = (value)=>{
+    let error
+    if(!value){
+      error="Required"
+    }
+    return error;
+}
 
 function NewYouTubeForm() {
   
@@ -26,33 +42,102 @@ function NewYouTubeForm() {
   <Formik
     initialValues={initialValues}
     validationSchema={validationSchema}
-    onSubmit={onSubmit}>
-
-    <div className='mt-5'>
-        <Form>
-
+    onSubmit={onSubmit}
+    validateOnChange={false}
+    validateOnBlur={true}
+    validateOnMount
+    >
+    {
+      (formik)=>{
+        return (
+          <Form>
           <div className='mb-4'>
             <label htmlFor='name'>Name</label>
             <Field type='text' id='name' name='name' />
-            <ErrorMessage name='name' />
+            <ErrorMessage name='name' component={TextError}/>
           </div> 
 
           <div className='mb-4'>
             <label htmlFor='email'>E-Mail</label>
             <Field type='email' id='email' name='email' />
-            <ErrorMessage name='email' />
+            <ErrorMessage name='email'>
+              {
+                (error)=>{
+                    return <div className='form-error'>{error}</div>
+                }
+              }
+            </ErrorMessage>
           </div>
 
           <div className='mb-4'>
             <label htmlFor='channel'>Channel</label>
             <Field type='text' id='channel' name='channel' />
-            <ErrorMessage name='channel' />
+            <ErrorMessage name='channel' component={TextError} />
+          </div>
+          
+          {/* Field level validation */}
+          <div className='mb-4'>
+            <label htmlFor='comments'>Comments</label>
+            <Field as='textarea' type='text' id='comments' name='comments' validate={validateComments} />
+            <ErrorMessage name='comments' component={TextError} />
           </div>
 
-          <button className='btn btn-primary' type='submit'>Submit</button>
+          <div className='mb-4'>
+            <label htmlFor='facebook'>FaceBook</label>
+            <Field as='input' id='facebook' type='text' name='social.facebook' />
+          </div>
+
+          <div className='mb-4'>
+            <label htmlFor='twitter'>Twitter</label>
+            <Field as='input' id='twitter' type='text' name='social.twitter' />
+          </div>
+
+          <div className='mb-4'>
+            <label htmlFor='priNumber'>primary Phone Number</label>
+            <Field as='input' id='priNumber' type='text' name='phoneNumbers[0]' />
+          </div>
+
+          <div className='mb-4'>
+            <label htmlFor='secNumber'>secondary Phone Number</label>
+            <Field as='input' id='secNumber' type='text' name='phoneNumbers[1]' />
+          </div>
           
-        </Form>
-    </div>
+          {/* For dynamic field alocation we have to use Render-Props Pattern */}
+          <div className='mb-4'>
+            <label htmlFor='phNumbers'>Phone Numbers</label>
+            <FieldArray name='phNumbers'>
+              {
+                (fieldArrayProps)=>{
+                  const {push, remove, form}=fieldArrayProps
+                  const {values} = form;
+                  const {phNumbers} = values;
+
+                  return (
+                    <div>
+                      {
+                        phNumbers.map((phNumber,index)=>(
+                          <div key={index}>
+                           <Field name={`phNumbers[${index}]}`} />
+                           <button type='button' onClick={()=>push('')}>Add Numbers</button>
+                           <button type='button' onClick={()=>remove(index)} >Remove Number</button>
+                          </div> 
+                        ))
+                      }
+                    </div>
+                  )
+                }
+              }
+            </FieldArray>
+          </div>
+
+          {/* <button className='btn btn-primary' disabled={!(formik.isValid && formik.dirty)} type='submit'>Submit</button> */}
+          <button className='btn btn-primary' disabled={formik.isSubmitting} type='submit'>Submit</button>
+          
+      </Form>
+        )
+      }
+    }
+      
   </Formik>
   );
 }
